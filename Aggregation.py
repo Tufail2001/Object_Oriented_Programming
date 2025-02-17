c4c0a20e8711ac0a4e3fe2a86c2a45d3027917d6
Hira urooj <Charleswalles2006@gmail.com>
2025-02-17 11:00:46 +0500

    Second Commit

--- Aggregation.py
+++ Aggregation.py
@@ -24,3 +24,27 @@ cust = Customer('Bilal', 'male', add)
 
 
 cust.print_address()
+
+
+# example 2
+class Customer:
+    def __init__(self, name, gender, address):
+        self.name = name
+        self.gender = gender
+        self.address = address
+    
+    def print_address(self):
+        print(self.address.city, self.address.pin, self.address.state)
+    
+    
+class Address:
+    def __init__(self, city, pin, state):
+        self.city = city
+        self.pin = pin
+        self.state = state
+        
+        
+add1 = Address('Isb', 345, 'Pak')      
+cust = Customer('Ali', 'male', add1)
+
+cust.print_address()
