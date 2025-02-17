f4cc861567ba2012ad1e9c0eb8234e8e5b476cf8
Hira urooj <Charleswalles2006@gmail.com>
2025-02-17 10:51:20 +0500

    Initial commit

---
+++ Aggegation.py
@@ -0,0 +1,26 @@
+# Aggregation: (has a relationship) , one class owns the other class. One class is owner and other is its property.
+#Customer -->has a --> Address
+#Restuarant --> has a --> Menu
+
+# example 
+class Customer:
+    def __init__(self, name, gender, address):
+        self.name = name
+        self.gender = gender
+        self.address = address
+        
+    def print_address(self):
+        print(self.address.city, ' ',self.address.pin, ' ', self.address.state)
+    
+class Address:
+    def __init__(self, city, pin, state):
+        self.city = city
+        self.pin = pin
+        self.state = state
+        
+add = Address('Taxila', 1239, 'Punjab')      
+        
+cust = Customer('Bilal', 'male', add)
+
+
+cust.print_address()
